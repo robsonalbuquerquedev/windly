e1a24e2e714bb62759bae448ff77ead550ff1c37
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-11-21 16:26:51 -0300

    feat: adicionar rota tables

---
+++ app/tables/page.tsx
@@ -0,0 +1,10 @@
+import CardTable from "@/components/tables/TableContent";
+
+export default function TablesPage() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold">Entendendo as Utilidades de Tabela</h1>
+            <CardTable />
+        </>
+    );
+}
