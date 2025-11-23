88d5f9ab0642a0c9971946c883e617fc94773c85
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-11-23 13:35:13 -0300

    update: atualizar rota responsividade-tailwind

--- app/blog/responsividade-tailwind/page.tsx
+++ app/blog/responsividade-tailwind/page.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import ResponsividadeTailwindContent from "@/components/blog/ResponsividadeTailwindContent";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
     title: {
