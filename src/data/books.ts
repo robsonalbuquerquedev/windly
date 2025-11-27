5721286cbcf5c873274ed9e2f3e37e97f1e97711
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-11-27 01:23:49 -0300

    feat: adicionar arquivos books.ts e book.ts

---
+++ src/data/books.ts
@@ -0,0 +1,46 @@
+import type { Book } from "@/types/book";
+
+export const BOOKS: Book[] = [
+    {
+        id: 1,
+        title: "Código Limpo",
+        description: "Um guia essencial para escrever códigos claros, eficientes e fáceis de manter.",
+        image: "/images/books/clean-code.jpg",
+        url: "https://amzn.to/3LVJeD3"
+    },
+    {
+        id: 2,
+        title: "Arquitetura Limpa",
+        description: "Aprenda como organizar projetos de forma profissional e sustentável.",
+        image: "/images/books/clean-architecture.jpg",
+        url: "https://amzn.to/49Jv7KU"
+    },
+    {
+        id: 3,
+        title: "O Programador Pragmático",
+        description: "Um clássico moderno que mostra princípios práticos para evoluir como desenvolvedor.",
+        image: "/images/books/pragmatic-programmer.jpg",
+        url: "https://amzn.to/43PDwsn"
+    },
+    {
+        id: 4,
+        title: "Estruturas de Dados e Algoritmos com JavaScript",
+        description: "O livro perfeito para dominar lógica, estruturas e algoritmos na prática.",
+        image: "/images/books/algorithms-js.jpg",
+        url: "https://amzn.to/3Xohfya"
+    },
+    {
+        id: 5,
+        title: "HTML e CSS: Projete e Construa Websites",
+        description: "Uma introdução visual e extremamente clara ao desenvolvimento web moderno.",
+        image: "/images/books/html-css.jpg",
+        url: "https://amzn.to/49Hg8kD"
+    },
+    {
+        id: 6,
+        title: "JavaScript e JQuery — Desenvolvimento de Interfaces",
+        description: "Um livro visual e didático para entender interatividade e front-end de verdade.",
+        image: "/images/books/js-jquery.jpg",
+        url: "https://amzn.to/48pli2h"
+    },
+];
---
+++ src/types/book.ts
@@ -0,0 +1,8 @@
+// /types/book.ts
+export interface Book {
+    id: number;
+    title: string;
+    description: string;
+    image?: string;
+    url: string;
+}
