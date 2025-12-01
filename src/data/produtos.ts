469af72ee09408378ec18d58795dd6d7bae756a6
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-11-30 22:54:14 -0300

    feat: adicionar arquivo produtos.ts

---
+++ src/data/produtos.ts
@@ -0,0 +1,131 @@
+// /data/products.ts
+import type { Product } from "@/types/product";
+
+export const PRODUCTS: Product[] = [
+    {
+        id: 1,
+        name: "Echo Dot (Geração mais recente)",
+        description: "Som potente, assistente de voz integrada.",
+        image: "/images/echodot.jpg",
+        url: "https://amzn.to/4p6VdfA"
+    },
+    {
+        id: 2,
+        name: "Kindle Paperwhite 16 GB",
+        description: "Leitura confortável com iluminação ajustável.",
+        image: "/images/kindle.jpg",
+        url: "https://amzn.to/4ok0MGo"
+    },
+    {
+        id: 3,
+        name: "TECLADO MECÂNICO GAMER",
+        description: "Compacto, resistente e ideal para uso diário.",
+        image: "/images/redragon.jpg",
+        url: "https://amzn.to/49BCZxU"
+    },
+    {
+        id: 4,
+        name: "Hub Usb 3.0 Baseus",
+        description: "Transforme sua conexão com alta velocidade, praticidade e eficiência.",
+        image: "/images/baseus.jpg",
+        url: "https://amzn.to/484JQPi"
+    },
+    {
+        id: 5,
+        name: "Mouse Sem Fio Logitech",
+        description: "O mouse favorito dos programadores — ergonomia e precisão.",
+        image: "/images/logitech.jpg",
+        url: "https://amzn.to/4pBUAuh"
+    },
+    {
+        id: 6,
+        name: "Monitor Gamer Samsung Odyssey",
+        description: "Mais espaço horizontal para produtividade e multitarefas.",
+        image: "/images/odyssey.jpg",
+        url: "https://amzn.to/3XRLTjz"
+    },
+    {
+        id: 7,
+        name: "Cadeira Ergonômica De Escritório",
+        description: "Conforto prolongado para longas horas de trabalho.",
+        image: "/images/thunderx3.png",
+        url: "https://mercadolivre.com/sec/1F8yqP3"
+    },
+    {
+        id: 8,
+        name: "Headset com fio USB Logitech H390",
+        description: "Áudio limpo e claro para reuniões e produtividade.",
+        image: "/images/logitech-h390.png",
+        url: "https://mercadolivre.com/sec/24oAW8B"
+    },
+    {
+        id: 9,
+        name: "Suporte Articulado para Monitor",
+        description: "Ajuste perfeito para ergonomia, organização e Instalação Simplificada.",
+        image: "/images/elg-f80.png",
+        url: "https://mercadolivre.com/sec/1gMLhFY"
+    },
+    {
+        id: 10,
+        name: "Teclado De Rede Bluetooth",
+        description: "Compacto, silencioso e perfeito para mobilidade.",
+        image: "/images/logitech-k380.png",
+        url: "https://mercadolivre.com/sec/2ZZXiEt"
+    },
+    {
+        id: 11,
+        name: "SSD Kingston NV2 NVMe 1TB",
+        description: "Alto desempenho para desenvolvimento e multitarefa.",
+        image: "/images/kingston-nv2.png",
+        url: "https://mercadolivre.com/sec/1qACeJQ"
+    },
+    {
+        id: 12,
+        name: "Lâmpada Smart RGB Wi-Fi Positivo",
+        description: "Automação simples para um ambiente moderno.",
+        image: "/images/lampada-smart.png",
+        url: "https://mercadolivre.com/sec/1WA7Vh8"
+    },
+    {
+        id: 13,
+        name: "Suporte Ajustável para Notebook",
+        description: "Melhore sua postura com um suporte compacto e dobrável.",
+        image: "/images/suporte-notebook-shopee.jpg",
+        url: "https://s.shopee.com.br/4AsRTM5cUN"
+    },
+    {
+        id: 14,
+        name: "Mousepad Gamer Grande",
+        description: "Superfície ampla e confortável para produtividade e jogos.",
+        image: "/images/mousepad-gamer-shopee.jpg",
+        url: "https://s.shopee.com.br/9UtxpQMmUr"
+    },
+    {
+        id: 15,
+        name: "Mini Teclado Mecânico 60%",
+        description: "Portátil, silencioso e perfeito para setups minimalistas.",
+        image: "/images/teclado-60-shopee.jpg",
+        url: "https://s.shopee.com.br/7KpTFi6Z1D"
+    },
+    {
+        id: 16,
+        name: "Hub USB-C Multifuncional",
+        description: "Expanda conexões no notebook com HDMI, USB 3.0 e SD.",
+        image: "/images/hub-typec-shopee.jpg",
+        url: "https://s.shopee.com.br/7V8tSGv8Qj"
+    },
+    {
+        id: 17,
+        name: "Webcam Full HD 1080p",
+        description: "Imagem clara e estável para reuniões e videochamadas.",
+        image: "/images/webcam-shopee.jpg",
+        url: "https://s.shopee.com.br/7AW33qVUQb"
+    },
+    {
+        id: 18,
+        name: "Ring Light com Tripé Ajustável",
+        description: "Iluminação suave para gravações, lives e videochamadas.",
+        image: "/images/ringlight-shopee.jpg",
+        url: "https://s.shopee.com.br/6ptCfQ1VrC"
+    }
+];
